bcdef07d973d3e0543f05e3833cdc25418c3432f
Ludwing Perez <mirage232@gmail.com>
2023-03-06 16:34:42 -0600

    Limpiar el proyecto antes de comenzar con el desarrollo de los componentes importantes.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const todos = [
+  {text: 'Tarea 1', completed: false},
+  {text: 'Tarea 2', completed: false},
+  {text: 'Tarea 3', completed: false},
+  {text: 'Tarea 4', completed: false},
+  {text: 'Tarea 5', completed: false}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      {/*
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {todos.map(todoItem => (<TodoItem />))}
+      </TodoList>
+      <CreateTodoButton />
+      */}
+    </React.Fragment>
+    
   );
 }
 
